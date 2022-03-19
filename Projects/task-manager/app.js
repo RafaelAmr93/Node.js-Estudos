751afcc78808f56610b1082d0abd8158a3589b50
RafaelAmr93 <rafaelamaro.993@gmail.com>
2022-03-19 12:43:12 -0300

    projects será um rep independente

--- Projects/task-manager/app.js
+++ Projects/task-manager/app.js
@@ -1,8 +1,21 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectionDB = require("./db/connect");
+require("dotenv").config();
 
-const port = 5000
+const port = 5000;
 
-app.listen(port, (req, res) =>{
-    console.log(`Listening on port ${port}...`)
-})
+//middleware
+app.use(express.json());
+
+//routes
+app.use("/api/v1/tasks", tasks);
+
+//db connection
+connectionDB(process.env.MONGO_URI);
+
+//server listening
+app.listen(port, (req, res) => {
+  console.log(`Listening on port ${port}...`);
+});
